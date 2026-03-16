5b79a26084bca7eef9dd96aaefa6f5dd172d8fbf
LugolBis <ld.desmares@gmail.com>
2026-03-16 15:16:28 +0100

    Try to optimize simulation

--- src/simulation.rs
+++ src/simulation.rs
@@ -10,7 +10,7 @@ use crate::{
 };
 
 const HEADER: &str = "alpha;arc_removed;stationnary_distrib_converge_time";
-const ALPHA_STEP: f64 = 0.005f64;
+const ALPHA_STEP: f64 = 0.01f64;
 const TRESHOLD_STEP: f64 = 0.005f64;
 
 pub fn simulation(
@@ -25,16 +25,22 @@ pub fn simulation(
     let mut buffer = BufWriter::new(file);
     writeln!(buffer, "{}", HEADER)?;
 
-    let mut first_iteration = true;
-
     let alpha_steps = (alpha_lim / ALPHA_STEP) as usize + 1;
     let treshold_steps = (treshold_lim / TRESHOLD_STEP) as usize + 1;
     let iterations = alpha_steps * treshold_steps;
     let mut counter = 1u64;
 
-    for alpha_c in 0..alpha_steps {
+    init(
+        treshold_steps,
+        epsilon,
+        matrix_path,
+        output_dir,
+        &mut counter,
+        &mut buffer,
+    )?;
+
+    for alpha_c in 1..alpha_steps {
         let alpha = alpha_c as f64 * ALPHA_STEP;
-        let mut prev_proba = 0f64;
 
         for treshold_c in 0..treshold_steps {
             print!("\r[Simulation : {}/{}]", counter, iterations);
@@ -45,19 +51,6 @@ pub fn simulation(
             if treshold == 0f64 {
                 // We just load the matrix without removing any edge
                 matrix = parse_file(matrix_path, market_parser, alpha, treshold)?;
-                prev_proba = 0f64;
-            } else if first_iteration {
-                if prev_proba > 0f64 {
-                    // We calculate `q` the treshold to remove more arcs based on the previous treshold : `prev_proba`
-                    let q = (treshold - prev_proba) / (1f64 - prev_proba);
-                    let path = output_dir.join(format!("{}.mtx", prev_proba));
-
-                    matrix = parse_file(&path, market_parser, alpha, q)?;
-                    prev_proba = treshold;
-                } else {
-                    matrix = parse_file(matrix_path, market_parser, alpha, treshold)?;
-                    prev_proba = treshold;
-                }
             } else {
                 // We just load the already calculated and writted matrix
                 let path = output_dir.join(format!("{}.mtx", treshold));
@@ -67,15 +60,52 @@ pub fn simulation(
             let (_, steps) = matrix.stationary_distribution(epsilon)?;
             writeln!(buffer, "{};{};{}", alpha, treshold, steps)?;
 
-            if first_iteration && treshold > 0f64 {
-                matrix.dump(&output_dir.join(format!("{}.mtx", treshold)))?;
-            }
-
             counter += 1;
         }
-
-        first_iteration = false;
     }
 
+    Ok(())
+}
+
+fn init(
+    treshold_steps: usize,
+    epsilon: f64,
+    matrix_path: &PathBuf,
+    output_dir: &PathBuf,
+    counter: &mut u64,
+    buffer: &mut BufWriter<File>,
+) -> Result<(), Box<dyn std::error::Error>> {
+    let mut prev_proba = 0f64;
+    let mut start = false;
+
+    for treshold_c in 0..treshold_steps {
+        let treshold = treshold_c as f64 * TRESHOLD_STEP;
+        let matrix: CSC;
+        if start {
+            matrix = parse_file(matrix_path, market_parser, 0f64, treshold)?;
+            start = false;
+        } else {
+            if prev_proba > 0f64 {
+                // We calculate `q` the treshold to remove more arcs based on the previous treshold : `prev_proba`
+                let q = (treshold - prev_proba) / (1f64 - prev_proba);
+                let path = output_dir.join(format!("{}.mtx", prev_proba));
+
+                matrix = parse_file(&path, market_parser, 0f64, q)?;
+                prev_proba = treshold;
+            } else {
+                matrix = parse_file(matrix_path, market_parser, 0f64, treshold)?;
+                prev_proba = treshold;
+            }
+        }
+
+        let (_, steps) = matrix.stationary_distribution(epsilon)?;
+        writeln!(buffer, "{};{};{}", 0f64, treshold, steps)?;
+
+        if treshold > 0f64 {
+            matrix.dump(&output_dir.join(format!("{}.mtx", treshold)))?;
+        }
+
+        *counter += 1;
+    }
     Ok(())
 }
